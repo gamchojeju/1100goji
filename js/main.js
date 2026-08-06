c91796c30d4f2c2f542830fbcd144a54b05e4556
감초식당 <kinopero@naver.com>
2026-08-07 07:00:01 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,42 +65,9 @@ threshold:0.15
 
 
 
-document.querySelectorAll(
-".story, .menu-card, .travel, .location"
-)
-.forEach(el=>{
+document.querySelectorAll(".hidden")
+.forEach(el => {
 
-
-el.classList.add("hidden");
-
-observer.observe(el);
-
+    observer.observe(el);
 
 });
-
-/* =========================
-   1100 GOJI
-   Build 0.50
-========================= */
-
-const hiddenElements = document.querySelectorAll(".hidden");
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if(entry.isIntersecting){
-
-            entry.target.classList.add("show");
-
-        }
-
-    });
-
-},{
-
-    threshold:0.15
-
-});
-
-hiddenElements.forEach(el => observer.observe(el));
