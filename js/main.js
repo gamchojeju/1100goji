cfe186bb6edfbef672238d2402f8febce9cb11a5
감초식당 <kinopero@naver.com>
2026-08-07 09:59:55 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,3 +71,19 @@ document.querySelectorAll(".hidden")
     observer.observe(el);
 
 });
+
+window.addEventListener("scroll",()=>{
+
+    const header=document.querySelector(".header");
+
+    if(window.scrollY>80){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
